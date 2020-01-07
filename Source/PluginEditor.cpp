684b5cf500254a48966814e874b4abfc715ddc1d
Kelvin Kellner <kelvinkellner123@gmail.com>
2020-01-06 22:48:37 -0500

    Set up knob graphic component
    The knob does nothing yet though, do not worry ;)

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,42 +1,139 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+  This is an automatically generated GUI class created by the Projucer!
 
-    It contains the basic framework code for a JUCE plugin editor.
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.5
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
 
   ==============================================================================
 */
 
-#include "PluginProcessor.h"
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
 #include "PluginEditor.h"
 
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
 //==============================================================================
-GainPluginAudioProcessorEditor::GainPluginAudioProcessorEditor (GainPluginAudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
+GainPluginAudioProcessorEditor::GainPluginAudioProcessorEditor (GainPluginAudioProcessor* ownerFilter)
+    : AudioProcessorEditor(ownerFilter)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
+
+    slider.reset (new Slider ("new slider"));
+    addAndMakeVisible (slider.get());
+    slider->setRange (0, 1, 0.1);
+    slider->setSliderStyle (Slider::Rotary);
+    slider->setTextBoxStyle (Slider::NoTextBox, false, 80, 20);
+    slider->setColour (Slider::thumbColourId, Colours::aquamarine);
+    slider->addListener (this);
+
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (600, 400);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
 }
 
 GainPluginAudioProcessorEditor::~GainPluginAudioProcessorEditor()
 {
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+    slider = nullptr;
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
 }
 
 //==============================================================================
 void GainPluginAudioProcessorEditor::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
 
-    g.setColour (Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    g.fillAll (Colours::cadetblue);
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
 }
 
 void GainPluginAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    slider->setBounds ((getWidth() / 2) - (256 / 2), (getHeight() / 2) - (256 / 2), 256, 256);
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
 }
+
+void GainPluginAudioProcessorEditor::sliderValueChanged (Slider* sliderThatWasMoved)
+{
+    //[UsersliderValueChanged_Pre]
+    //[/UsersliderValueChanged_Pre]
+
+    if (sliderThatWasMoved == slider.get())
+    {
+        //[UserSliderCode_slider] -- add your slider handling code here..
+        //[/UserSliderCode_slider]
+    }
+
+    //[UsersliderValueChanged_Post]
+    //[/UsersliderValueChanged_Post]
+}
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Projucer information section --
+
+    This is where the Projucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="GainPluginAudioProcessorEditor"
+                 componentName="" parentClasses="public AudioProcessorEditor, public Timer"
+                 constructorParams="GainPluginAudioProcessor* ownerFilter" variableInitialisers="AudioProcessorEditor(ownerFilter)"
+                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
+                 fixedSize="0" initialWidth="600" initialHeight="400">
+  <BACKGROUND backgroundColour="ff5f9ea0"/>
+  <SLIDER name="new slider" id="1b385c7265a9bdf9" memberName="slider" virtualName=""
+          explicitFocusOrder="0" pos="0Cc 0Cc 256 256" thumbcol="ff7fffd4"
+          min="0.0" max="1.0" int="0.1" style="Rotary" textBoxPos="NoTextBox"
+          textBoxEditable="1" textBoxWidth="80" textBoxHeight="20" skewFactor="1.0"
+          needsCallback="1"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,35 +1,69 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+  This is an automatically generated GUI class created by the Projucer!
 
-    It contains the basic framework code for a JUCE plugin editor.
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.5
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
 
   ==============================================================================
 */
 
 #pragma once
 
+//[Headers]     -- You can add your own extra header files here --
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "PluginProcessor.h"
+//[/Headers]
+
+
 
 //==============================================================================
 /**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
 */
-class GainPluginAudioProcessorEditor  : public AudioProcessorEditor
+class GainPluginAudioProcessorEditor  : public AudioProcessorEditor,
+                                        public Timer,
+                                        public Slider::Listener
 {
 public:
-    GainPluginAudioProcessorEditor (GainPluginAudioProcessor&);
+    //==============================================================================
+    GainPluginAudioProcessorEditor (GainPluginAudioProcessor* ownerFilter);
     ~GainPluginAudioProcessorEditor();
 
     //==============================================================================
-    void paint (Graphics&) override;
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
     void resized() override;
+    void sliderValueChanged (Slider* sliderThatWasMoved) override;
+
+
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    GainPluginAudioProcessor& processor;
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
 
+    //==============================================================================
+    std::unique_ptr<Slider> slider;
+
+
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GainPluginAudioProcessorEditor)
 };
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
